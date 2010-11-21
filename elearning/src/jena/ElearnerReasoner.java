58eac99ea2016befdc0a41c20096e61cfe2242f7
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2010-11-21 06:44:07 +0000

    config set and database operation.

---
+++ elearning/src/jena/ElearnerReasoner.java
@@ -0,0 +1,129 @@
+package jena;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import util.Constant;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+import exception.ConceptNotExistInModelException;
+/*************************************************************
+ * Elearner Reasoner is the class used for reasoning.
+ * The rules, ontology classes and the properties of the model are the param of this class.
+ * 
+ * @author William
+ *
+ */
+public class ElearnerReasoner {
+
+	public ElearnerReasoner(){
+		
+	}
+	public ElearnerReasoner(File owlFile,File ruleFile){
+		this.owlFile = owlFile;
+		this.ruleFile = ruleFile;
+	}
+	
+	/********************************************************************
+	 * the initial model location is set in the file Constant which locates in the util package.
+	 * @return
+	 ******************************************************************/
+	public ArrayList<Individual> getAllConcepts(){
+		OntModel model = OwlFactory.getOntOWLModel();
+		ArrayList <Individual> concepts = new ArrayList<Individual>();
+		OntClass concept = model.getOntClass(Constant.NS+"E_Concept");
+		Iterator <Individual>iter2 = model.listIndividuals();
+		while(iter2.hasNext()){
+			Individual indi= (Individual) iter2.next();
+			if(concept.equals(indi.getOntClass())){
+				concepts.add(indi);
+				System.out.println(indi.getLocalName());
+			}
+		}
+		return concepts;
+	}
+	public ArrayList<Individual> getAllResources(){
+		OntModel model = OwlFactory.getOntOWLModel();
+		ArrayList <Individual> resources = new ArrayList<Individual>();
+		OntClass resource = model.getOntClass(Constant.NS+"E_Resource");
+		Iterator <Individual>iter2 = model.listIndividuals();
+		while(iter2.hasNext()){
+			Individual indi= (Individual) iter2.next();
+			if(indi==null)break;
+			if(resource.equals(indi.getOntClass())){
+				resources.add(indi);
+				System.out.println(indi.getLocalName());
+			}
+		}
+		return resources;
+	}
+	public ArrayList<Individual> getPreConcepts(Resource concept,OntModel model) throws ConceptNotExistInModelException{
+		/*
+		if(!hasConcept(concept,model)){
+			throw new ConceptNotExistInModelException("the model doesn't contain the concept"+concept.getLocalName());
+		}*/
+		Resource ontConcept = model.getResource(Constant.NS+"RIA");
+		SimpleSelector selector = new SimpleSelector(concept, model.getProperty(Constant.NS+"is_post_concept_of"), (RDFNode)ontConcept);
+		StmtIterator iter = model.listStatements(selector);
+		
+		ArrayList <Individual> concepts = new ArrayList<Individual>();
+		while(iter.hasNext()){
+			Statement s = iter.nextStatement();
+			System.out.println(s);
+			Individual indi= (Individual) iter.next();
+			System.out.println("ms");
+				concepts.add(indi);
+				System.out.println(indi.getLocalName());
+		}
+		return concepts;
+	}
+	public boolean hasConcept(Individual concept,OntModel model){
+		Individual con = model.getIndividual(concept.getURI());
+		if(con ==null){
+			return false;
+		}
+		return true;
+	}
+	
+	
+	public InfModel getInfModel() {
+		return infModel;
+	}
+	public void setInfModel(InfModel infModel) {
+		this.infModel = infModel;
+	}
+	public OntModel getOntModel() {
+		return ontModel;
+	}
+	public void setOntModel(OntModel ontModel) {
+		this.ontModel = ontModel;
+	}
+	public File getOwlFile() {
+		return owlFile;
+	}
+	public void setOwlFile(File owlFile) {
+		this.owlFile = owlFile;
+	}
+	public File getRuleFile() {
+		return ruleFile;
+	}
+	public void setRuleFile(File ruleFile) {
+		this.ruleFile = ruleFile;
+	}
+
+
+	private InfModel infModel;
+	private OntModel ontModel;
+	private File owlFile;
+	private File ruleFile;
+}
--- elearning/src/util/Constant.java
+++ elearning/src/util/Constant.java
@@ -1,11 +1,24 @@
 package util;
 
 public class Constant {
+	/*******************************************************************
+	 * Jena Reasoning Config
+	 *********************************************************************/
 	public static String NS ="http://www.elearning.com/";
 	public static String OWLFile ="D:\\EclipseWorkspace\\elearning\\protege\\elearning.owl";
 	public static String RulesFile="D: \\EclipseWorkspace\\elearning\\src\\jena\\elearning.rules";
 	
-	
+	/********************************************************************
+	 * Database Connection Config
+	 ********************************************************************/
+	public static String DBURL = "jdbc:mysql://localhost:3306/elearning";        
+    public static String DBUSER = "root";
+    public static String DBPASSWORD = "ms";
+    
+    
+	/**********************************************************************
+	 * To Be Used, but now useless....
+	 **********************************************************************/
 	public static String [] post={"post","associate professor","instructor"};
 	public static String [] ERESOURCE_TYPES={
 		"E_Resource_AUDIO","E_Resource_CODE","E_Resource_CAJ","E_Resource_DOC","E_Resource_PDF","E_Resource_TXT",
@@ -13,8 +26,6 @@ public class Constant {
 	public static String [] EDUCATION_TYPES={
 		"book","paper","lecture"
 	};
-	public static String [] titles={"porofessor"};
-	public static void println(String s){
-		System.out.println(s);
-	}
+	public static String [] titles={"professor"};
+
 }
