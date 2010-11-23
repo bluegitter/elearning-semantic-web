627773bd7b0d23f2d2c347cc628c299c819f5ccd
Williamma.WM <Williamma.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2010-11-23 08:56:43 +0000

    use SPARQL to query the result

---
+++ elearning/src/jena/ElearnerQuery.java
@@ -0,0 +1,91 @@
+package jena;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import util.Constant;
+
+public class ElearnerQuery {
+	public static void main(String [] args) throws IOException{
+		getAllConceptName();
+	}
+	public static ArrayList<String> getAllConceptName()throws IOException{
+		// Open the bloggers RDF graph from the filesystem
+		InputStream in = new FileInputStream(new File(Constant.OWLFile));
+
+		// Create an empty in-memory model and populate it from the graph
+		Model model = ModelFactory.createMemModelMaker().createModel(null);
+		model.read(in,null); // null base URI, since model URIs are absolute
+		in.close();
+
+		// Create a new query
+		String queryString = 
+			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+			"PREFIX base: <http://www.owl-ontologies.com/e-learning.owl#> " +
+			"SELECT ?concept " +
+			"WHERE {" +
+			"      ?concept rdf:type base:E_Concept . " +
+			"      }";
+
+		Query query = QueryFactory.create(queryString);
+
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+		ArrayList<String> concepts = new ArrayList<String>();
+		while(results.hasNext()){
+			QuerySolution qs = results.next();
+			//trim() eliminates the blank space behind the uri, which make the getResource() method working.
+			String uri = qs.get("?concept").toString().trim();
+			String name = model.getResource(uri).getLocalName();
+			concepts.add(name);
+		}
+		qe.close();
+		return concepts;
+	}
+	public static void test() throws IOException{
+		// Open the bloggers RDF graph from the filesystem
+		InputStream in = new FileInputStream(new File(Constant.OWLFile));
+
+		// Create an empty in-memory model and populate it from the graph
+		Model model = ModelFactory.createMemModelMaker().createModel(null);
+		model.read(in,null); // null base URI, since model URIs are absolute
+		in.close();
+
+		// Create a new query
+		String queryString = 
+			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+			"PREFIX base: <http://www.owl-ontologies.com/e-learning.owl#> " +
+			"SELECT ?concept ?result " +
+			"WHERE {" +
+			"      ?concept rdf:type base:E_Concept . " +
+			"      ?concept base:id ?result . "+
+			"      }";
+
+		Query query = QueryFactory.create(queryString);
+
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+		 
+		// Output query results	
+		ResultSetFormatter.out(System.out, results, query);
+		
+		// Important - free up resources used running the query
+		qe.close();
+	}
+}
