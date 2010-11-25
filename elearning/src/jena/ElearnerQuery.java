0b90beec909137f11cdec576b0956e2853a09d48
Williamma.WM <Williamma.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2010-11-25 09:54:05 +0000

    本体文件修改： 1）增加本体文件输入限制，比如ID只允许输入一个值，在Forms栏中进行选择，将索要限定的本体的属性的输入框更改为SingleResourceWidget即可。 2）根据中国软件工程学科教程知识点录入了Concept实例。（曾经卉卉整理的内容，我直接敲入OWL文件里了。逐步添加。） 3）删除了E_personal本体，一共就三个属性name,email,address.没必要单独出来，所以，直接加入E_People里。 4)Transtive属性在使用

--- elearning/src/jena/ElearnerQuery.java
+++ elearning/src/jena/ElearnerQuery.java
@@ -16,12 +16,13 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
 
 import util.Constant;
 
 public class ElearnerQuery {
 	public static void main(String [] args) throws IOException{
-		getAllConceptName();
+		test();
 	}
 	public static ArrayList<String> getAllConceptName()throws IOException{
 		// Open the bloggers RDF graph from the filesystem
@@ -31,7 +32,6 @@ public class ElearnerQuery {
 		Model model = ModelFactory.createMemModelMaker().createModel(null);
 		model.read(in,null); // null base URI, since model URIs are absolute
 		in.close();
-
 		// Create a new query
 		String queryString = 
 			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
@@ -57,11 +57,9 @@ public class ElearnerQuery {
 		qe.close();
 		return concepts;
 	}
-	public static void test() throws IOException{
-		// Open the bloggers RDF graph from the filesystem
+	
+	public static void getPartOfConcept(String conceptName) throws IOException{
 		InputStream in = new FileInputStream(new File(Constant.OWLFile));
-
-		// Create an empty in-memory model and populate it from the graph
 		Model model = ModelFactory.createMemModelMaker().createModel(null);
 		model.read(in,null); // null base URI, since model URIs are absolute
 		in.close();
@@ -70,10 +68,36 @@ public class ElearnerQuery {
 		String queryString = 
 			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
 			"PREFIX base: <http://www.owl-ontologies.com/e-learning.owl#> " +
-			"SELECT ?concept ?result " +
+			"SELECT ?concept " +
 			"WHERE {" +
 			"      ?concept rdf:type base:E_Concept . " +
-			"      ?concept base:id ?result . "+
+			"      ?concept base:is_part_of base:Database ."+
+			"      }";
+
+		Query query = QueryFactory.create(queryString);
+
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+		// Output query results	
+		ResultSetFormatter.out(System.out, results, query);
+		
+		qe.close();
+	}
+	public static void test() throws IOException{
+		// Open the bloggers RDF graph from the filesystem
+		InputStream in = new FileInputStream(new File(Constant.OWLFile));
+
+		// Create an empty in-memory model and populate it from the graph
+		Model model = OwlFactory.getGenericRuleReasonerModel();
+		// Create a new query
+		String queryString = 
+			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+			"PREFIX base: <http://www.owl-ontologies.com/e-learning.owl#> " +
+			"SELECT ?concept " +
+			"WHERE {" +
+			"      ?concept rdf:type base:E_Concept . " +
+			"      ?concept base:is_part_of base:Software_Engineer . "+
 			"      }";
 
 		Query query = QueryFactory.create(queryString);
