8c439794e0241b985a658feb8f2b83e9fedfc0de
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2010-12-19 08:07:27 +0000

    update the old version

---
+++ elearning/src/datastructure/ConceptTree.java
@@ -0,0 +1,39 @@
+package datastructure;
+
+import java.util.ArrayList;
+
+import ontology.EConcept;
+
+public class ConceptTree {
+	private EConcept root;
+	public ArrayList<EConcept> concepts;
+	public ConceptTree(){
+		root = new EConcept("root","root");
+		concepts = new ArrayList<EConcept>();
+	}
+	public int getSize(){
+		return concepts.size();
+	}
+	public boolean hasSon(String concept){
+		for(EConcept con : concepts){
+			if(con.getName().equals(concept)){
+				return false;
+			}
+		}
+		return true;
+	}
+	public boolean addFather(String concept){
+		if(hasSon(concept)){
+			return false;
+		}else{
+			int id = concepts.size()+1;
+			EConcept newCon = new EConcept("cid"+id);
+			newCon.setName(concept);
+			return true;
+		}
+	}
+	public boolean  addSon(){
+		return false;
+	}
+
+}
