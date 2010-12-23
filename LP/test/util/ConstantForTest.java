5df282f1521a3dded7a75cbd3a0fb832a6b56a8e
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2010-12-23 04:11:43 +0000

    test performance of writing

---
+++ LP/test/util/ConstantForTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package util;
+
+/**
+ *
+ * @author william
+ */
+public class ConstantForTest {
+    public static String TestOwlFileLocation = "test\\owl\\";
+}
