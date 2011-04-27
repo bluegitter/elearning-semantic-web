c432d9c7424f55c8b1c6d5a7686fbb155d1f08de
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2011-04-27 09:06:28 +0000

    owl update- delete concept

--- LP/src/jena/impl/ELearnerOntologyImpl.java
+++ LP/src/jena/impl/ELearnerOntologyImpl.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -23,10 +24,13 @@ import util.Constant;
 public class ELearnerOntologyImpl {
     public OWLOntologyManager manager;
     public OWLOntology ontology;
+    public OWLDataFactory factory ;
+
     public IRI documentIRI;
     public ELearnerOntologyImpl(){
         try {
             manager = OWLManager.createOWLOntologyManager();
+            factory = manager.getOWLDataFactory();
             ontology = manager.loadOntologyFromOntologyDocument(new File(Constant.OWLFile));
         } catch (OWLOntologyCreationException ex) {
             Logger.getLogger(ELearnerOntologyImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -35,6 +39,7 @@ public class ELearnerOntologyImpl {
     public ELearnerOntologyImpl(File owlFile){
         try {
             manager = OWLManager.createOWLOntologyManager();
+            factory = manager.getOWLDataFactory();
             ontology = manager.loadOntologyFromOntologyDocument(owlFile);
         } catch (OWLOntologyCreationException ex) {
             Logger.getLogger(ELearnerOntologyImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -50,5 +55,10 @@ public class ELearnerOntologyImpl {
 
     public void addEConcept(){
         
+    }
+    
+    public static void main(String[] args) {
+        ELearnerOntologyImpl eoi = new ELearnerOntologyImpl();
+
     }
 }
