9d0e136c31246cb7021402768ceaddabbe23b0d2
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2011-05-03 17:28:07 +0000

    admin panel test

---
+++ LP/test/lp/TestAdminOperationPane.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lp;
+
+import java.util.HashSet;
+import lp.admin.TableTemplate;
+
+/**
+ *
+ * @author William Ma <williamma.wm@gmail.com>
+ */
+public class TestAdminOperationPane {
+    public static void main2(String[] args) {
+        javax.swing.JFrame f = new javax.swing.JFrame();
+        f.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+       // AdminOperatorPane pane = new AdminOperatorPane();
+        TableTemplate pane = new TableTemplate();
+        HashSet<Object> num = new HashSet<Object>();
+        for(int i = 0;i<25;i++){
+            num.add(new Integer(i));
+        }
+        pane.setData(num);
+        pane.initTable();
+        f.add(pane);
+        f.pack();
+        f.setVisible(true);
+
+    }
+    public static void main(String args[]){
+        javax.swing.JFrame f = new javax.swing.JFrame();
+        f.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        AdminOperatorPane pane = new AdminOperatorPane();
+        f.add(pane);
+        f.pack();
+        f.setVisible(true);
+    }
+}
