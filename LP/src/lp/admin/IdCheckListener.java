893bde18624e7938f7bac9e7b8217e6934b42799
WilliamMa.WM <WilliamMa.WM@ba09d43b-7106-7bc7-13ab-fa16c34b3180>
2011-05-12 10:25:15 +0000

    admin panel

---
+++ LP/src/lp/admin/IdCheckListener.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lp.admin;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import jena.impl.ELearnerModelImpl;
+
+/**
+ *
+ * @author student
+ */
+public class IdCheckListener implements ListSelectionListener {
+
+    public static int ADD_CONCEPT_ID = 1;
+    public static int ADD_RESOURCE_ID = 2;
+    public static int ADD_GOAL_ID = 3;
+    private ELearnerModelImpl emi;
+    private int type;
+
+    public IdCheckListener(int type, ELearnerModelImpl emi) {
+        this.type = type;
+        this.emi = emi;
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        switch (type) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
